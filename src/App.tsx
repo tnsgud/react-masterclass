8ba207e5e52c2ee52bfc1edac5dbdc82369d37cd
tnsgud <qkrtnsgud0229@gmail.com>
2024-03-17 18:14:31 +0900

    add motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
   return (
-    <Container>
-      <H1>Hello world</H1>
-    </Container>
+    <Wrapper>
+      <Box
+        initial={{ scale: 0 }}
+        transition={{ type: 'tween' }}
+        animate={{
+          scale: 1,
+          rotateZ: 360,
+        }}
+      />
+    </Wrapper>
   );
 }
 
