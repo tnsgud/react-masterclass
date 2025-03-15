4ce6afaa9fbba9dcb6ca55e71fa4a3972c1dbedd
tnsgud <60930113+tnsgud@users.noreply.github.com>
2025-03-15 14:39:30 +0000

    6.16 Selectors part One

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSetRecoilState } from "recoil";
 import { IToDo, toDoState } from "../atoms";
 
+const food = ['pizza', 'mango', 'kimchi', 'kimbab']
+
 function ToDo({ text, category, id }: IToDo) {
   const setToDos = useSetRecoilState(toDoState);
   const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
@@ -11,9 +13,13 @@ function ToDo({ text, category, id }: IToDo) {
 
     setToDos((oldToDos) => {
       const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
-      const oldToDo = oldToDos[targetIndex];
-      const newToDo = { text, id, category: name };
-      return oldToDos;
+      const newToDo = { text, id, category: name as any };
+
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
     });
   };
 
